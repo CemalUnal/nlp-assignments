72f93979c0554518f7ed254ce9d3a45ccb142505
b21328538 <b21328538@cs.hacettepe.edu.tr>
2018-05-09 11:31:11 +0300

    Update readme & add more comments

--- Exp4/README.md
+++ Exp4/README.md
@@ -11,40 +11,89 @@ _**Number:** 21328538_
 
 In this assignment I have used Python programming language (Python 3) along with TensorFlow (tensorflow 1.8.0) library.
 
+## Algorithm
+- Read all sentences and tag current sentence as ``[1,0]`` if the meaning of it is _positive_ or tag it as ``[0,1]`` if the meaning of it is _negative_. And store them in a list
+- Shuffle them using `numpy.random.shuffle`.
+- Split the tagged sentences according to a given percentage. Store the first part as train data and store the remaining part as test data.
+- Read the vectors file and store the each word with its vectors in a `dictionary`.
+- Then create 4 matrices. If we think that the given percentage is 75 and since there are 149 sentences in total containing positive and negatives. These matrices are;
+  - Train matrix
+    - It is a 111x200 matrix that contains vector sums for each word in each train sentence.
+  - Train matrix with tags
+    - It is a 111x2 matrix that contains tags for each train sentence. ([1,0]  for the positives and [0,1] for the negatives)
+  - Test matrix
+    - It is a 38x200 matrix that contains vector sums for each word in each train sentence.
+  - Test matrix with tags
+    - It is a 38x2 matrix that contains tags for each test sentence. ([1,0]  for the positives and [0,1] for the negatives)
+- Call the function to implement multilayer perceptron.
+- The TensorFlow functions used in this implementor function are described unde the below title. 
+
 ## TensorFlow Functions and Utilities That I Used in My Assignment
 
 #### tf.placeholder
-In the TensorFlow documentation it says that `Placeholder inserts a placeholder for a tensor that will be always fed.` In simple words, a placeholder is simply a variable that we will assign data to at a later date. In my code, I did not know what the value of the array `x` and `y` would be during the declaration phase. So I defined them as placeholders.
+In the TensorFlow documentation it says that **_Placeholder inserts a placeholder for a tensor that will be always fed._** In simple words, a placeholder is simply a variable that we will assign data to at a later date. In my code, I did not know what the value of the array `training_data_placeholder` and `output_data_placeholder` would be during the declaration phase. So I defined them as placeholders.
 
-For example `training_data_placeholder = tf.placeholder(tf.float32, [None, 200])` and `output_data_placeholder = tf.placeholder(tf.float32, [None, 2])`. Since I am not making any initialization in the declaration, TensorFlow needs to know what data type each element within the tensor is going to be. I am providing it by using `tf.float32`. `[None, 200]` is used to specify the shape of the data that will injected to this variable. I have used **None** since the size of the train and test matrices are different. And TensorFlow will decide it later on while execution.
+For example
+- `training_data_placeholder = tf.placeholder(tf.float32, [None, 200])`
+- `output_data_placeholder = tf.placeholder(tf.float32, [None, 2])`
 
-#### tf.Variable
+Since I am not making any initialization in the declaration, TensorFlow needs to know what data type each element within the tensor is going to be. I am providing it by using `tf.float32`. `[None, 200]` is used to specify the shape of the data that will injected to this variable. I have used _**None**_ since the size of the train and test matrices are different. And TensorFlow will decide it later on while execution.
 
-#### tf.random_normal
+#### tf.Variable and tf.random_normal
+I have declared weights and bias values for the connections between input layer and first hidden layer, between other hidden layers, and lastly between last hidden layer and the output layer.
+For example below two line represents the weight and bias value between input layer and first hidden layer.
+- `w1 = tf.Variable(tf.random_normal([200, number_of_neurons], stddev=0.03), name='w1')`
+- `b1 = tf.Variable(tf.random_normal([number_of_neurons]), name='b1')`
 
-#### tf.add
+I have initialized the values of the weights using a random normal distribution with a standard deviation 0.03.
+**tf.random_normal** Gives us random values from a normal distribution. Its usage can be seen from the above code fragment.
 
-#### tf.nn.relu
+#### tf.add, tf.matmul and tf.nn.relu
+In the first line, we execute the standard matrix multiplication of the weights (W1) by the input vector x and we add the bias b1.
+- `hidden_layer_out_one = tf.add(tf.matmul(training_data_placeholder, w1), b1)`
+- `hidden_layer_out_one = tf.nn.relu(hidden_layer_out_one)`
+
+**tf.matmul** allows us to perform matrix multiplication easily. And using the **tf.add** we can add our result matrix and bias value.
+Using **tf.nn.relu**, I applied _**ReLU (Rectified Linear Unit)**_ activation function to the output of each hidden layer.
 
 #### tf.nn.softmax
+We were expecting to apply softmax to the output layer in this assignment, so I have used **tf.nn.softmax** as the following;
 
-#### tf.clip_by_value
+- `output_layer = tf.nn.softmax(tf.add(tf.matmul(hidden_layer_out_two, w3), b3))`
 
-#### tf.reduce_mean
+#### tf.clip_by_value, tf.reduce_mean, tf.reduce_sum and tf.log
+It is used to make sure that we will never get log(0) during the training phase.
 
-#### tf.reduce_sum
+- `output_clipped = tf.clip_by_value(output_layer, 1.1754943508222875e-38, 0.9999999)`
 
-#### tf.log
+It converts the **output_layer** to a clipped version, result is between 1.1754943508222875e-38 and 0.999999.
+
+To calculate the cross entropy, I have used below code;
+
+`cross_entropy = -tf.reduce_mean(tf.reduce_sum(output_data_placeholder * tf.log(output_clipped) + (1 - output_data_placeholder) * tf.log(1 - output_clipped), axis=1))`
+
+**tf.reduce_sum** basically takes the sum of a given axis of the tensor we are supplying to it.
 
 #### tf.train.GradientDescentOptimizer
+As the optimization method, we were expecting to use Gradient Descent. I have used **tf.train.GradientDescentOptimizer** to implement Gradient Descent. You can see the following code;
+
+- ` optimiser = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cross_entropy)`
+
+I have initialized it with a learning_rate. And as can be seen above, optimizer is trying to minimize the **cross_entropy**.
 
 #### tf.global_variables_initializer
+tf.global_variables_initializer returns an operator that initializes global variables.
+- `initialization_operator = tf.global_variables_initializer()`
 
-#### tf.equal
+#### tf.equal, tf.argmax and tf.cast
+`correct_prediction = tf.equal(tf.argmax(output_data_placeholder, 1), tf.argmax(output_layer, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))`
 
-#### tf.argmax
-
-#### tf.cast
+**tf.equal** simply returns true if the given two parameters are equal, false otherwise.
+**tf.argmax** returns the index of the maximum value in the given vector.
+**tf.cast** is used to cast correct_prediction from boolean to TensorFlow float
 
 #### tf.Session
-session.run'i  filan burada anlat.
+A Session object encapsulates the environment in which Operation objects are executed, and Tensor objects are evaluated. With using **tf.Session()**, I have started the session.
+
+With usage of **session.run**, I easily executed previously declared operators.
--- Exp4/src/assignment4.py
+++ Exp4/src/assignment4.py
@@ -1,34 +1,32 @@
 import sys
 import numpy as np
-from string import punctuation
 import string
 import tensorflow as tf
+
 
 def implement_multi_layer_perceptron(train_mtrx, train_mtrx_w_tags, test_mtrx, test_mtrx_w_tags):
     learning_rate = 0.001
     training_epoch = 10
     number_of_neurons = 100
-    # number_of_hidden_layers = 2 # simdilik hep 2 gibi dusunecegim
-    # ama sonradan dinamik almak icin kodu degistirmek gerekecek
 
-    x = tf.placeholder(tf.float32, [None, 200])
-    y = tf.placeholder(tf.float32, [None, 2])
+    training_data_placeholder = tf.placeholder(tf.float32, [None, 200])
+    output_data_placeholder = tf.placeholder(tf.float32, [None, 2])
 
     # Declare the weights connecting the input to the hidden layer
     # initialise the values of the weights using a random normal distribution.
     # random_normal allows us to create a matrix of a given size that is initialized with random samples
-    w1 = tf.Variable(tf.random_normal([200, number_of_neurons], stddev=0.03), name='W1')
+    w1 = tf.Variable(tf.random_normal([200, number_of_neurons], stddev=0.03), name='w1')
     b1 = tf.Variable(tf.random_normal([number_of_neurons]), name='b1')
 
     # Declare the weights connecting first hidden layer to the second hidden layer
-    w2 = tf.Variable(tf.random_normal([number_of_neurons, 200], stddev=0.03), name='W2')
+    w2 = tf.Variable(tf.random_normal([number_of_neurons, 200], stddev=0.03), name='w2')
     b2 = tf.Variable(tf.random_normal([200]), name='b2')
 
-    w3 = tf.Variable(tf.random_normal([200, 2], stddev=0.03), name='W3')
+    w3 = tf.Variable(tf.random_normal([200, 2], stddev=0.03), name='w3')
     b3 = tf.Variable(tf.random_normal([2]), name='b3')
 
     # Calculate the output of the first hidden layer
-    hidden_layer_out_one = tf.add(tf.matmul(x, w1), b1)
+    hidden_layer_out_one = tf.add(tf.matmul(training_data_placeholder, w1), b1)
     hidden_layer_out_one = tf.nn.relu(hidden_layer_out_one)
 
     # Calculate the output of the second hidden layer
@@ -39,41 +37,58 @@ def implement_multi_layer_perceptron(train_mtrx, train_mtrx_w_tags, test_mtrx, t
     # After tha apply softmax activation
     output_layer = tf.nn.softmax(tf.add(tf.matmul(hidden_layer_out_two, w3), b3))
 
-    # It is to make sure we will never get log(0) during training
+    # It is to make sure that we will never get log(0) during training
     output_clipped = tf.clip_by_value(output_layer, 1e-10, 0.9999999)
-    cross_entropy = -tf.reduce_mean(tf.reduce_sum(y * tf.log(output_clipped) + (1 - y) * tf.log(1 - output_clipped),
-                                                  axis=1))
+    cross_entropy = -tf.reduce_mean(tf.reduce_sum(output_data_placeholder * tf.log(output_clipped)
+                                                  + (1 - output_data_placeholder) * tf.log(1 - output_clipped)
+                                                  , axis=1))
     # Gradient Descent optimizer
     # We are telling that we want to minimize the cross entropy
     optimiser = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
 
-    # setup the variable initialisation
-    initialize_operations = tf.global_variables_initializer()
+    # setup the operator for variable initialisation
+    initialization_operator = tf.global_variables_initializer()
 
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(output_layer, 1))
+    correct_prediction = tf.equal(tf.argmax(output_data_placeholder, 1), tf.argmax(output_layer, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+    # create the session
     with tf.Session() as sess:
-        # initialise the variables
-        sess.run(initialize_operations)
+        sess.run(initialization_operator)
+
         for epoch in range(training_epoch):
-            sess.run([optimiser, cross_entropy], feed_dict={x: train_mtrx, y: train_mtrx_w_tags})
-        print(sess.run(accuracy, feed_dict={x: test_mtrx, y: test_mtrx_w_tags}))
+            sess.run([optimiser, cross_entropy], feed_dict={training_data_placeholder: train_mtrx,
+                                                            output_data_placeholder: train_mtrx_w_tags})
+
+        print(sess.run(accuracy, feed_dict={training_data_placeholder: test_mtrx,
+                                            output_data_placeholder: test_mtrx_w_tags}))
 
 
 def strip_punctuation(s):
+    """
+        Removes punctuations from the given sentence and replaces these punctuations with a space character.
+    """
     replace_punctuation = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
     return s.translate(replace_punctuation)
 
 
-def read_sentences(sentences_file, l_sentences, sentences_w_tags, p_or_n):
+def read_sentences(sentences_file, l_sentences, sentences_w_tags, positive_or_negative):
+    """
+        It creates a tag according to sentence type.
+        This tag is
+            - [1, 0] for the positive sentences
+            - [0, 1] for the negative sentences
+        Then opens the sentences file.
+        For each line in these files, it removes punctuations and adds current sentence to the list
+        Stores the each sentence with its tag in a dictionary.
+    """
     tag = []
 
-    if p_or_n == "positives":
+    if positive_or_negative == "positives":
         tag.append(1)
         tag.append(0)
 
-    elif p_or_n == "negatives":
+    elif positive_or_negative == "negatives":
         tag.append(0)
         tag.append(1)
 
@@ -82,11 +97,18 @@ def read_sentences(sentences_file, l_sentences, sentences_w_tags, p_or_n):
             line = line.strip()
             line = strip_punctuation(line)
 
+            # add current sentence to the list
             l_sentences.append(line)
+            # add current sentence with its tag to the dictionary
             sentences_w_tags[line] = tag
 
 
 def read_vectors_file(file):
+    """
+        Opens vectors file.
+        For each line in the vectors file, it separates the word from its vectors.
+        Stores the each word with its vectors in a dictionary.
+    """
     words_and_vectors = {}
     all_vectors = []
 
@@ -99,22 +121,31 @@ def read_vectors_file(file):
             temp_vectors = split_result[1]
 
             all_vectors = temp_vectors.split(' ')
-
             all_vectors = [float(f) for f in all_vectors]
 
+            # add current word with its vector to the dictionary
             words_and_vectors[_word] = all_vectors
 
     return words_and_vectors, len(all_vectors)
 
 
 def split_list(shuf_sentences, t_percentage):
+    """
+        Splits a list into two pieces according to the given percentage and returns these two pieces.
+    """
     index = int(round(t_percentage * len(shuf_sentences)))
     return shuf_sentences[index:], shuf_sentences[:index]
 
 
 def create_train_and_test_matrices(matrix, matrix_with_tags, sentences, vec_size):
+    """
+        Splits a list according to the given percentage.
+    """
     sentence_index = 0
-    # for each sentence in the training and test set
+
+    # for each sentence in the training and test set,
+    # calculate vectors by summing up the vector of each word.
+    # then store the sentences with vectors in a matrix.
     for sentence in sentences:
         # create and initialize sum of vectors array
         # it will reinitialized for each sentence
@@ -125,6 +156,7 @@ def create_train_and_test_matrices(matrix, matrix_with_tags, sentences, vec_size
         current_sentence_words = sentence.split()
         # for each word in the current sentence
         for word in current_sentence_words:
+            # get the vector of the current word in the current sentence
             vector = words_and_their_vectors.get(word)
 
             if vector is not None:
@@ -132,6 +164,7 @@ def create_train_and_test_matrices(matrix, matrix_with_tags, sentences, vec_size
 
         matrix[sentence_index] = sum_of_vectors
 
+        # get the tag of the current sentence
         tag_of_current_sentence = sentences_with_tags.get(sentence)
 
         if tag_of_current_sentence is not None:
@@ -174,4 +207,3 @@ if __name__ == "__main__":
     create_train_and_test_matrices(test_matrix, test_matrix_with_tags, test_sentences, vector_size)
 
     implement_multi_layer_perceptron(train_matrix, train_matrix_with_tags, test_matrix, test_matrix_with_tags)
-
